8d9e3a3ae51170bb51454cb4de74e8c89aedc262
Shweedes <metior22848@mail.ru>
2025-10-18 11:21:00 +0300

    add task10.1

---
+++ tasks/task10/script.js
@@ -0,0 +1,39 @@
+// 1) Создайте функцию, которая принимает в себя целое число минут и возвращает время в нужном формате строки. (Смотри пример). Обратите внимание на окончание слова "час" - оно меняется в зависимости от цифры. Если вместо аргумента приходит не число, дробное или отрицательное число - функция возвращает строку "Ошибка, проверьте данные"
+//
+// Внимание! Давайте пока ограничимся максимум 600ю минутами (10 часов). Так как проверки на большие числа будут раздувать код (33 часа, 31 час, 11 часов и тд). Этого будет достаточно и код будет проверять именно этот промежуток (1 - 10 часов). Но вы можете реализовать и полный скрипт, он тоже должен проходить тесты.
+//
+//     Пример:
+//
+// getTimeFromMinutes(150) => "Это 2 часа и 30 минут"
+//
+// getTimeFromMinutes(50) => "Это 0 часов и 50 минут"
+//
+// getTimeFromMinutes(0) => "Это 0 часов и 0 минут"
+//
+// getTimeFromMinutes(-150) => "Ошибка, проверьте данные"
+
+function getTimeFromMinutes(minutes) {
+    let resultString = ''
+    let hours = 0;
+    let resultMinutes
+
+    if(minutes < 0 || isNaN(minutes) || !Number.isInteger(minutes)) {
+        return "Ошибка, проверьте данные"
+    }
+
+    if(minutes >= 60) {
+        hours = Math.floor(minutes / 60);
+        resultMinutes = minutes - hours * 60
+        if(hours > 1 && hours < 5) {
+            return resultString += `Это ${hours} часа и ${resultMinutes} минут`
+        } else if(hours === 1) {
+            return resultString += `Это ${hours} час и ${resultMinutes} минут`
+        }
+    } else {
+        resultMinutes = minutes;
+    }
+
+    return resultString += `Это ${hours} часов и ${resultMinutes} минут`
+}
+
+console.log(getTimeFromMinutes(0))
